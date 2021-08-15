a5083e1a77b5d0b21c838758f61bf62613cb9e0a
Md Arif Hossain <arif.swfu@outlook.com>
2021-08-15 15:37:42 +0800

    initial files for client and server

---
+++ client/src/components/AboutPage/Info.jsx
@@ -0,0 +1,11 @@
+const Info = () => {
+    const name = 'Info';
+
+    return (
+        <div>
+            <h1>{name} Section</h1>
+        </div>
+    );
+};
+
+export default Info;
---
+++ server/src/configs/db.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+let DB_URL;
+if (process.env.DB_MODE === "development") {
+  DB_URL = process.env.DB_URL_DEV;
+} else if (process.env.DB_MODE === "production") {
+  DB_URL = process.env.DB_URL_PRODUCTION;
+}
+
+const database = () => {
+  mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() =>
+      console.log(`Database connected in ${process.env.DB_MODE} mode`)
+    )
+    .catch((err) => console.log(err.message));
+};
+
+export default database;
