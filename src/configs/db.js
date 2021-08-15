cac030f7e23fc40a90935882bd7584691c96bb23
Md Arif Hossain <arif.swfu@outlook.com>
2021-08-16 00:32:43 +0800

    employee routes done with mysql

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,77 +1,40 @@
+import Sequelize from "sequelize";
+import Employee from "../models/Employee.js";
+
 import { config } from "dotenv";
-import mongoose from "mongoose";
-import mysql from "mysql";
 config();
 
-let MONGODB_URL;
-if (process.env.NODE_MODE === "development") {
-  MONGODB_URL = process.env.MONGODB_URL_DEV;
-} else if (process.env.NODE_MODE === "production") {
-  MONGODB_URL = process.env.MONGODB_URL_PRODUCTION;
-}
-
-// <============ mongodb ============>
-const mongodbConnect = () => {
-  mongoose
-    .connect(MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-    .then(() =>
-      console.log(`MongoDB database connected in ${process.env.NODE_MODE} mode`)
-    )
-    .catch((err) => console.log(err.message));
+const dbConfig = {
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "",
+  DB: process.env.DB_NAME || "hr_management",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
 
-// <============ mysql ============>
-const mysqlConnection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
 });
 
-// const mysqlRemoteConnection = mysql.createPool({
-//   connectionLimit: 100,
-//   host: process.env.DB_REMOTE_HOST,
-//   port: 3306,
-//   user: process.env.DB_REMOTE_USER,
-//   password: process.env.DB_REMOTE_PASSWORD,
-//   database: process.env.DB_REMOTE_NAME,
-// });
+const db = {};
 
-const mysqlRemoteConnection2 = mysql.createPool({
-  connectionLimit: 100,
-  host: "db4free.net",
-  port: 3306,
-  user: "devarif",
-  password: "Arif12345",
-  database: "hr_devarif",
-});
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.employees = Employee(sequelize, Sequelize);
 
-const mysqlConnect = () => {
-  mysqlConnection.connect((err) => {
-    if (err) {
-      console.log(
-        `Error connecting to MySQL: ${JSON.stringify(err, undefined, 2)}`
-      );
-    } else {
-      console.log(`MySQL database connected in ${process.env.NODE_MODE} mode`);
-    }
-  });
-};
-
-const mysqlRemoteConnect = function (callback) {
-  mysqlRemoteConnection2.getConnection(function (err, conn) {
-    if (err) {
-      console.log(
-        `Error connecting to MySQL: ${JSON.stringify(err, undefined, 2)}`
-      );
-    } else {
-      console.log(`MySQL database connected in REMOTE mode`);
-    }
-  });
-};
-
-export { mongodbConnect, mysqlConnect, mysqlRemoteConnect, mysqlConnection };
+export default db;
---
+++ src/models/Employee.js
@@ -0,0 +1,20 @@
+const Employee = (sequelize, Sequelize) => {
+  const employee = sequelize.define("employee", {
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+
+  return employee;
+};
+
+export default Employee;
